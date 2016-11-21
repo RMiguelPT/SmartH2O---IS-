daf9060de6b1c4a4b0ed3f820cc728ea5b339336
gilberto1991 <gilberto_marcelino@live.com.pt>
2016-11-21 21:36:36 +0000

    Add files via upload

---
+++ SmartH2O_DU/SmartH2O_DU/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using uPLibrary.Networking.M2Mqtt;
+
+namespace SmartH2O_DU
+{
+    class Program
+    {
+        MqttClient m_cClient;
+        static void Main(string[] args)
+        {
+
+            SensorNodeDll.SensorNodeDll dll = new SensorNodeDll.SensorNodeDll();
+
+            m_cClient = new MqttClient(IPAddress.Parse("192.168.237.226"));
+
+            m_cClient.Connect(Guid.NewGuid().ToString());
+            if (!m_cClient.IsConnected)
+            {
+                Console.WriteLine("Error connecting to message broker...");
+                return;
+            }
+
+            dll.Initialize(recebeDados, 1500);
+
+            Console.ReadKey();
+            if (m_cClient.IsConnected)
+            {
+                // m_cClient.Unsubscribe(m_strTopicsInfo);
+                m_cClient.Disconnect();
+
+
+            }
+            dll.Stop();
+
+        }
+        static public void recebeDados(string str)
+        {
+
+            
+            string[] words = str.Split(';');
+            string topic = words[1];
+            string value = words[2];
+            Console.WriteLine(topic);
+            
+            //string[] m_strTopicsInfo = { "news", "complaints" };
+           
+            m_cClient.Publish(topic, Encoding.UTF8.GetBytes(value));
+            //;
+           
+
+        }
+        }
+    
+}
